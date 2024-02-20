883d79c18702c2e0977ee921b4c1a3594e28437a
Guillermo Silva <guillermitus@gmail.com>
2024-02-20 11:11:53 -0300

    making components reusable

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -1,6 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -9,14 +10,26 @@ function getRandomInt(max) {
 function Header() {
   const description = reactDescriptions[getRandomInt(2)];
 
-  return(<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>)
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt="alt" />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function App() {
@@ -24,6 +37,18 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              image={componentsImg}
+            />
+            <CoreConcept  title="Props" />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
