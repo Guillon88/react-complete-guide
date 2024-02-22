0e79c72834fd435fc5dee83e5e78cd63838687f2
Guillermo Silva <guillermitus@gmail.com>
2024-02-22 16:41:27 -0300

    deriving and outputting data

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -8,9 +8,11 @@ import Header from './components/Header/Header.jsx';
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
+import { EXAMPLES } from './data.js'
+
 function App() {
 
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -41,10 +43,14 @@ function App() {
             <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
             <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('satate')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>{EXAMPLES[selectedTopic].code}</pre>
+          </div>
         </section>
-        {selectedTopic}
       </main>
     </div>
   );
