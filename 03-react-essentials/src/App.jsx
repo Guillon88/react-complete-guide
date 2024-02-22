9e038a270c15c968a22ccefd2070afca61a8099e
Guillermo Silva <guillermitus@gmail.com>
2024-02-22 17:33:16 -0300

    option 3

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -17,6 +17,17 @@ function App() {
     console.log(selectedTopic);
   }
 
+  let tabContent = <p>Please select a topic.</p>
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>{EXAMPLES[selectedTopic].code}</pre>
+            </div>
+    )
+  }
+
   return (
     <div>
       <Header />
@@ -44,15 +55,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-
-          {!selectedTopic && <p>Please select a topic.</p> }
-          {selectedTopic &&
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>{EXAMPLES[selectedTopic].code}</pre>
-            </div>
-          }
+          {tabContent}
         </section>
       </main>
     </div>
