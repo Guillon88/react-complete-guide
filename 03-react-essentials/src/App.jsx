77d8636e6d3a884fcdead4532dc660301ebf6cae
Guillermo Silva <guillermitus@gmail.com>
2024-03-01 21:18:15 -0300

    CoreConcepts

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import componentsImg from "./assets/components.png";
-import { CORE_CONCEPTS } from "./data.js";
-
 import Header from "./components/Header/Header.jsx";
-
-import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
-
+import CoreConcepts from "./components/CoreConcepts";
 import { EXAMPLES } from "./data.js";
 
 function App() {
@@ -32,44 +28,8 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map( (conceptItem) => (
-              <CoreConcept {...conceptItem } />
-            ) )}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        
       </main>
     </>
   );
