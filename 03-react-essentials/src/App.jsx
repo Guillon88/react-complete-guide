df4f8c8bec1c4fe281762566d793fccc1b84b2c6
Guillermo Silva <guillermitus@gmail.com>
2024-03-01 21:24:31 -0300

    Examples comp

--- 03-react-essentials/src/App.jsx
+++ 03-react-essentials/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from "react";
-import componentsImg from "./assets/components.png";
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
 import CoreConcepts from "./components/CoreConcepts";
-import { EXAMPLES } from "./data.js";
+import Examples from "./components/Examples.jsx";
+
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
 
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>{EXAMPLES[selectedTopic].code}</pre>
-      </div>
-    );
-  }
 
   return (
     <>
       <Header />
       <main>
         <CoreConcepts />
-        
+        <Examples />
       </main>
     </>
   );
